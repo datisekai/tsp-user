622b4ec663502d7d2deea671754756ccf709036a
Thành Đạt <datly030102@gmail.com>
2024-10-26 18:40:01 +0700

    fix: deploy

--- deploy.sh
+++ deploy.sh
@@ -9,12 +9,12 @@ docker build -t $IMAGE_NAME .
 # Tạo container tạm thời để sao chép file
 CONTAINER_ID=$(docker create $IMAGE_NAME)
 
-rm -rf ~/public/tsp-user
+rm -rf /var/www/html/tsp-user
 
 # Sao chép nội dung từ /public/tsp-user trong container ra thư mục ~/public bên ngoài
-docker cp $CONTAINER_ID:/public/tsp-user ~/public
+docker cp $CONTAINER_ID:/public/tsp-user /var/www/html/tsp-user
 
 # Xoá container tạm thời
 docker rm $CONTAINER_ID
 
-echo "Build complete and files copied to ~/public"
+echo "Build complete and files copied to /var/www/html/tsp-user"
