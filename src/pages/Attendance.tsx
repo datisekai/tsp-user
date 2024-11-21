f107476327b239261f023e8982c6e6c0b54235b0
Finn <datly030102@gmail.com>
2024-11-21 17:49:52 +0700

    feat: add filter for exam

--- src/pages/Attendance.tsx
+++ src/pages/Attendance.tsx
@@ -1,108 +1,127 @@
-import {useEffect, useMemo, useState} from "react";
+import { useEffect, useMemo, useState } from "react";
 import MyTable from "../components/UI/MyTable";
-import {attendanceSchemas} from "../dataTable/attendanceTable";
-import {useAttendanceStore, useCommonStore} from "../stores";
+import { attendanceSchemas } from "../dataTable/attendanceTable";
+import { useAttendanceStore, useCommonStore } from "../stores";
 import MyCalendar from "../components/UI/MyCalendar.tsx";
-import {Card} from "primereact/card";
-import {Dropdown} from "primereact/dropdown";
-import {Button} from "primereact/button";
-import {useClassStore} from "../stores/classStore.ts";
-import {exportToExcel, getPastDate} from "../utils";
+import { Card } from "primereact/card";
+import { Dropdown } from "primereact/dropdown";
+import { Button } from "primereact/button";
+import { useClassStore } from "../stores/classStore.ts";
+import { exportToExcel, getPastDate } from "../utils";
 import dayjs from "dayjs";
 
-
 const Attendance = () => {
-    const {resetActions, setHeaderTitle, isLoadingApi, setHeaderActions} = useCommonStore();
-    const {getAll, attendances, total} = useAttendanceStore();
-    const [dates, setDates] = useState<any>(null)
-    const [selectedClass, setSelectedClass] = useState(null);
-    const {getMe, classes} = useClassStore()
+  const { resetActions, setHeaderTitle, isLoadingApi, setHeaderActions } =
+    useCommonStore();
+  const { getAll, attendances, total } = useAttendanceStore();
+  const [dates, setDates] = useState<any>(null);
+  const [selectedClass, setSelectedClass] = useState(null);
+  const { getMe, classes } = useClassStore();
 
-    useEffect(() => {
-        setHeaderTitle("Lịch sử điểm danh");
-        getMe()
-        setHeaderActions([
-            {
-                icon: 'pi pi-download',
-                title: "Export excel",
-                onClick: () => {
-                    const dataExcel = attendances.map(item => ({
-                        "Môn học": item.attendance.class.major.name,
-                        "Lớp học": item.attendance.class.name,
-                        "Giảng viên": item.attendance.class.teachers.map(t => t.name).join(', '),
-                        "Thời gian": dayjs(item.createdAt).format('DD/MM/YYYY HH:mm'),
-                    }))
-                    exportToExcel(dataExcel, `DIEM_DANH_${Date.now()}.xlsx`)
-                }
-            }
-        ])
-        return () => {
-            resetActions();
-        };
-    }, []);
+  useEffect(() => {
+    setHeaderTitle("Lịch sử điểm danh");
+    getMe({ pagination: false });
+    setHeaderActions([
+      {
+        icon: "pi pi-download",
+        title: "Export excel",
+        onClick: () => {
+          const dataExcel = attendances.map((item) => ({
+            "Môn học": item.attendance.class.major.name,
+            "Lớp học": item.attendance.class.name,
+            "Giảng viên": item.attendance.class.teachers
+              .map((t) => t.name)
+              .join(", "),
+            "Thời gian": dayjs(item.createdAt).format("DD/MM/YYYY HH:mm"),
+          }));
+          exportToExcel(dataExcel, `DIEM_DANH_${Date.now()}.xlsx`);
+        },
+      },
+    ]);
+    return () => {
+      resetActions();
+    };
+  }, []);
 
-    const classOptions = useMemo(() => {
-        return classes.map(item => ({
-            name: `${item.major.name} - ${item.name}`,
-            value: item.id
-        }))
-    }, [classes])
+  const classOptions = useMemo(() => {
+    return classes.map((item) => ({
+      name: `${item.major.name} - ${item.name}`,
+      value: item.id,
+    }));
+  }, [classes]);
 
-    const applyFilter = () => {
-        const query: any = {}
-        if (selectedClass) {
-            query.classId = selectedClass
-        }
-        if (dates) {
-            query.from = dates[0].toISOString();
-            query.to = dates[1].toISOString();
-        }
-
-        getAll(query)
+  const applyFilter = () => {
+    const query: any = {};
+    if (selectedClass) {
+      query.classId = selectedClass;
+    }
+    if (dates) {
+      query.from = dates[0].toISOString();
+      query.to = dates[1].toISOString();
     }
 
-    const resetFilter = () => {
-        setSelectedClass(null);
-        setDates(null)
-        getAll({})
-    }
+    getAll(query);
+  };
 
-    return (
-        <div className={'tw-space-y-4'}>
-            <Card title={'Tìm kiếm'}>
-                <div className={"tw-flex tw-items-end tw-gap-4"}>
-                    <div>
-                        <div className={"mb-1"}>Lọc theo môn</div>
-                        <Dropdown
-                            value={selectedClass} onChange={(e) => setSelectedClass(e.value)} options={classOptions}
-                            optionLabel="name"
-                            loading={isLoadingApi}
-                            placeholder="Chọn lớp học" className="w-full"/>
-                    </div>
-                    <div>
-                        <div className={"mb-1"}>Lọc theo ngày</div>
-                        <MyCalendar initToday={getPastDate(30)} spaceDate={30} dates={dates} setDates={setDates}/>
-                    </div>
-                    <div>
-                        <div className={"mb-1"}>Thao tác</div>
-                        <div className={"tw-flex tw-gap-2"}>
-                            <Button onClick={applyFilter} icon={'pi pi-play'} iconPos={"right"}
-                                    label={"Áp dụng"}></Button>
-                            <Button onClick={resetFilter} severity={"contrast"} icon={"pi pi-refresh"} iconPos={'right'}
-                                    label={"Reset"}></Button>
-                        </div>
-                    </div>
+  const resetFilter = () => {
+    setSelectedClass(null);
+    setDates(null);
+    getAll({});
+  };
 
-                </div>
-            </Card>
-            <MyTable
-                schemas={attendanceSchemas}
-                data={attendances}
-                totalRecords={total}
-                onChange={(query) => getAll(query)}
+  return (
+    <div className={"tw-space-y-4"}>
+      <Card title={"Tìm kiếm"}>
+        <div className={"tw-flex tw-items-end tw-gap-4"}>
+          <div>
+            <div className={"mb-1"}>Lọc theo môn</div>
+            <Dropdown
+              value={selectedClass}
+              onChange={(e) => setSelectedClass(e.value)}
+              options={classOptions}
+              optionLabel="name"
+              loading={isLoadingApi}
+              placeholder="Chọn lớp học"
+              className="w-full"
             />
+          </div>
+          <div>
+            <div className={"mb-1"}>Lọc theo ngày</div>
+            <MyCalendar
+              initToday={getPastDate(30)}
+              spaceDate={30}
+              dates={dates}
+              setDates={setDates}
+            />
+          </div>
+          <div>
+            <div className={"mb-1"}>Thao tác</div>
+            <div className={"tw-flex tw-gap-2"}>
+              <Button
+                onClick={applyFilter}
+                icon={"pi pi-play"}
+                iconPos={"right"}
+                label={"Áp dụng"}
+              ></Button>
+              <Button
+                onClick={resetFilter}
+                severity={"contrast"}
+                icon={"pi pi-refresh"}
+                iconPos={"right"}
+                label={"Reset"}
+              ></Button>
+            </div>
+          </div>
         </div>
-    );
+      </Card>
+      <MyTable
+        schemas={attendanceSchemas}
+        data={attendances}
+        totalRecords={total}
+        onChange={(query) => getAll(query)}
+      />
+    </div>
+  );
 };
 
 export default Attendance;
--- src/pages/Exam/index.tsx
+++ src/pages/Exam/index.tsx
@@ -1,50 +1,113 @@
-import {useEffect} from "react";
+import { useEffect, useMemo, useState } from "react";
 import MyTable from "../../components/UI/MyTable.tsx";
-import {pathNames} from "../../constants";
-import {examSchemas} from "../../dataTable/examTable.tsx";
-import {useCommonStore} from "../../stores";
-import {useExamStore} from "../../stores/examStore.ts";
-import {useNavigate} from "react-router-dom";
-
+import { pathNames } from "../../constants";
+import { examSchemas } from "../../dataTable/examTable.tsx";
+import { useCommonStore } from "../../stores";
+import { useExamStore } from "../../stores/examStore.ts";
+import { useNavigate } from "react-router-dom";
+import { Card } from "primereact/card";
+import { Dropdown } from "primereact/dropdown";
+import { Button } from "primereact/button";
+import { useClassStore } from "../../stores/classStore.ts";
 
 const Index = () => {
-    const {resetActions, setHeaderTitle} = useCommonStore();
-    const {getAll, exams, total} = useExamStore();
-    const navigate = useNavigate();
+  const { resetActions, setHeaderTitle, isLoadingApi } = useCommonStore();
+  const { getAll, exams, total } = useExamStore();
+  const [selectedClass, setSelectedClass] = useState(null);
+  const navigate = useNavigate();
+  const { getMe, classes } = useClassStore();
 
-    useEffect(() => {
-        setHeaderTitle("Bài kiểm tra");
-        return () => {
-            resetActions();
-        };
-    }, []);
-    return (
-        <MyTable
-            schemas={examSchemas}
-            data={exams}
-            totalRecords={total}
-            actions={[
-                {
-                    icon: "pi-sign-in",
-                    tooltip: "Tham gia",
-                    severity: 'info',
-                    isHidden: (record) => record.status != "active",
-                    onClick: (data) => {
-                        navigate(pathNames.JOIN_EXAM.replace(":id", data.id));
-                    },
-                },
-                {
-                    icon: "pi-info-circle",
-                    tooltip: "Chi tiết",
-                    isHidden: (record) => record.status == "active" || !record?.showResult,
-                    onClick: (data) => {
-                        navigate(pathNames.HISTORY_EXAM.replace(":id", data.id));
-                    },
-                },
-            ]}
-            onChange={(query) => getAll(query)}
-        />
-    );
+  useEffect(() => {
+    setHeaderTitle("Bài kiểm tra");
+    getMe({ pagination: false });
+    return () => {
+      resetActions();
+    };
+  }, []);
+
+  const applyFilter = () => {
+    const query: any = {};
+    if (selectedClass) {
+      query.classId = selectedClass;
+    }
+
+    getAll(query);
+  };
+
+  const classOptions = useMemo(() => {
+    return classes.map((item) => ({
+      name: `${item.major.name} - ${item.name}`,
+      value: item.id,
+    }));
+  }, [classes]);
+
+  const resetFilter = () => {
+    setSelectedClass(null);
+    getAll({});
+  };
+  return (
+    <div className={"tw-space-y-4"}>
+      <Card title={"Tìm kiếm"}>
+        <div className={"tw-flex tw-items-end tw-gap-4"}>
+          <div>
+            <div className={"mb-1"}>Lọc theo môn</div>
+            <Dropdown
+              value={selectedClass}
+              onChange={(e) => setSelectedClass(e.value)}
+              options={classOptions}
+              optionLabel="name"
+              loading={isLoadingApi}
+              placeholder="Chọn lớp học"
+              className="w-full"
+            />
+          </div>
+          <div>
+            <div className={"mb-1"}>Thao tác</div>
+            <div className={"tw-flex tw-gap-2"}>
+              <Button
+                onClick={applyFilter}
+                icon={"pi pi-play"}
+                iconPos={"right"}
+                label={"Áp dụng"}
+              ></Button>
+              <Button
+                onClick={resetFilter}
+                severity={"contrast"}
+                icon={"pi pi-refresh"}
+                iconPos={"right"}
+                label={"Reset"}
+              ></Button>
+            </div>
+          </div>
+        </div>
+      </Card>
+      <MyTable
+        schemas={examSchemas}
+        data={exams}
+        totalRecords={total}
+        actions={[
+          {
+            icon: "pi-sign-in",
+            tooltip: "Tham gia",
+            severity: "info",
+            isHidden: (record) => record.status != "active",
+            onClick: (data) => {
+              navigate(pathNames.JOIN_EXAM.replace(":id", data.id));
+            },
+          },
+          {
+            icon: "pi-info-circle",
+            tooltip: "Chi tiết",
+            isHidden: (record) => record.status == "active",
+            onClick: (data) => {
+              navigate(pathNames.HISTORY_EXAM.replace(":id", data.id));
+            },
+          },
+        ]}
+        onChange={(query) => getAll(query)}
+      />
+    </div>
+  );
 };
 
 export default Index;
